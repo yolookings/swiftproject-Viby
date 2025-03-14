4e223d03f2f74f9e8c2335a6090d793628bbe8a2
Maulana Zahiri <147925317+yolookings@users.noreply.github.com>
2025-03-14 10:32:54 +0700

    Initial Commit

---
+++ Viby/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Viby
+//
+//  Created by Maulana Ahmad Zahiri on 14/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VibyTests/VibyTests.swift
@@ -0,0 +1,17 @@
+//
+//  VibyTests.swift
+//  VibyTests
+//
+//  Created by Maulana Ahmad Zahiri on 14/03/25.
+//
+
+import Testing
+@testable import Viby
+
+struct VibyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
